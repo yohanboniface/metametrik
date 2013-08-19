26a95c85ccb91af98aba626ee8b6de71e72bbfce
Yohan Boniface <yb@enix.org>
2013-08-19 11:55:42 +0200

    cleaning import2es (cf okfn/metametrik#6)

--- bin/import2es.py
+++ bin/import2es.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
+"""Import metametrik structured files into ElasticSearch
+
+Usage:
+    import2es.py <filepath>... [--flush]
+    import2es.py --dummydata
+"""
 
 import os
 import simplejson
 
+from docopt import docopt
+
 from pyelasticsearch import ElasticSearch
+from pyelasticsearch.exceptions import ElasticHttpNotFoundError
+
 
 es = ElasticSearch('http://localhost:9200/')
 
 
-def run(data):
-    es.delete_index('papers')
-    mapping = {
-        'paper': {
-        }
-    }
-    mapping = {
+def flush():
+    try:
+        es.delete_index('papers')
+    except ElasticHttpNotFoundError:
+        pass
+    mappings = {
         'paper': {
             'properties': {
                 'dependent': {"type": "string", "analyzer": "keyword"},
@@ -28,15 +37,27 @@ def run(data):
             }
         }
     }
-    es.create_index('papers', settings={'mappings': mapping})
-    es.health(wait_for_status='yellow')
+    es.create_index('papers', settings={'mappings': mappings})
+
+
+def run(data):
     es.bulk_index('papers', 'paper', data)
     es.refresh('papers')
 
 if __name__ == '__main__':
-    filepath = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        'sample.json'
-    )
-    with open(filepath) as f:
-        run(simplejson.loads(f.read()))
+    arguments = docopt(__doc__)
+    files = []
+    if arguments['--dummydata']:
+        filepath = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'sample.json'
+        )
+        files = [filepath]
+    if arguments['<filepath>']:
+        files = arguments['<filepath>']
+    if arguments['--flush'] or arguments['--dummydata']:
+        flush()
+    es.health(wait_for_status='yellow')
+    for filepath in files:
+        with open(filepath) as f:
+            run(simplejson.loads(f.read()))
